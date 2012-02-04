6a8a8d285819a28d0da8c5e3c69b19f4dfaecd51
yuqi <coolcnid@gmail.com>
2012-02-04 23:54:07 +0000

    add example02 content, also make the both scala file compiled by scalac

--- example01.scala
+++ example01.scala
@@ -1,63 +1,76 @@
-class Calculator(brand:String) {
-	var color: String = if (brand == "TI") {
-		"blue"
-	} else if (brand == "HP") {
-		"black"
-	} else {
-		"white"
-	}
+/*
+ * you can run this file using 
+ * 1. uncomment the last line, then scala example01.scala
+ * 2. scalac example01.scala;scala  then enter example01()
+ */
+object example01 {
+        class Calculator(brand:String) {
+            var color: String = if (brand == "TI") {
+                "blue"
+            } else if (brand == "HP") {
+                "black"
+            } else {
+                "white"
+            }
 
-	//var brand: String = "HP"
-	def add(m: Int, n: Int): Int = m + n
+            //var brand: String = "HP"
+            def add(m: Int, n: Int): Int = m + n
+        }
+
+    def apply() {
+
+        var calc = new Calculator("HP")
+
+        println(calc.add(1, 2))
+
+        //println(calc.brand)
+        println(calc.color)
+
+        class ScientificCalculator(brand: String) extends Calculator(brand) {
+            def log(m: Double, base: Double) = math.log(m) / math.log(base)
+        }
+
+        class EvenMoreScientificCalculator(brand: String) extends ScientificCalculator(brand) {
+            def log(m: Int):Double = log(m, math.exp(1))
+        }
+
+        var esCalc = new EvenMoreScientificCalculator("HP"); 
+        println(esCalc.log(12));
+
+        def outter() {
+            var abc = "abc";
+
+            def inner() {
+                println("inner called", abc);
+            }
+
+            abc = "efg"
+            println("outter called")
+            return inner;
+        }
+
+        outter();
+
+        // Types
+        trait Cache[K, V] {
+            def get(key: K): V
+            def put(key: K, value: V)
+            def delete(key: K)
+        }
+
+        class MyCache extends Cache[String, String] {
+            def get(key: String): String = {
+                println("get is called")
+                return "value";
+            }
+            def put(key: String, value: String) = {}
+            def delete(key: String) = {}
+        }
+
+        val myCache = new MyCache
+        myCache.get("me")
+    }
 }
 
-var calc = new Calculator("HP")
-
-println(calc.add(1, 2))
-
-//println(calc.brand)
-println(calc.color)
-
-class ScientificCalculator(brand: String) extends Calculator(brand) {
-	def log(m: Double, base: Double) = math.log(m) / math.log(base)
-}
-
-class EvenMoreScientificCalculator(brand: String) extends ScientificCalculator(brand) {
-	def log(m: Int):Double = log(m, math.exp(1))
-}
-
-var esCalc = new EvenMoreScientificCalculator("HP"); 
-println(esCalc.log(12));
-
-def outter() {
-	var abc = "abc";
-
-	def inner() {
-		println("inner called", abc);
-	}
-
-	abc = "efg"
-	println("outter called")
-	return inner;
-}
-
-outter();
-
-// Types
-trait Cache[K, V] {
-	def get(key: K): V
-	def put(key: K, value: V)
-	def delete(key: K)
-}
-
-class MyCache extends Cache[String, String] {
-  def get(key: String): String = {
-		println("get is called")
-	  return "value";
-	}
-	def put(key: String, value: String) = {}
-	def delete(key: String) = {}
-}
-
-val myCache = new MyCache
-myCache.get("me")
+// this is for execute directly
+// example01()
--- example02.scala
+++ example02.scala
@@ -1,3 +1,19 @@
+object example02 {
+	def apply() {
+/*
+ * It looks like you're trying to declare the package membership in a Scala
+ * script (run using the scala command) or in the REPL.
+ *
+ * Only files defining just classes and objects which are compiled with scalac
+ * may be defined as belonging to a package.
+ *
+ * When you run code in a script or a REPL session, behind the scenes it is
+ * actually compiled inside a method of an object, in which scope a package
+ * declaration wouldn't be legal.
+ *
+ */
+// package com.twitter.example
+
 class Bar {
 	def apply() = 0
 }
@@ -46,3 +62,54 @@ class AddOne extends Function[Int, Int] {
 
 val plusOne = new AddOne()
 println("plusOne(3)" + plusOne(3))
+
+
+object colorHolder {
+	val BLUE = "Blue"
+	val RED = "Red"
+}
+
+// see top for more information on the reason
+// println("this colour is:" + com.twitter.example.colorHolder.BLUE)
+
+val times = 1
+
+val matched = times match {
+	case 1 => "one"
+	case 2 => "two"
+	case _ => "SOME OTHER VALUE"
+}
+
+println("matched:" + matched)
+
+/* this bit of code in scala twitter page, is not working
+ * as the match expect match class, not boolean
+def calcType(calc: example01.Calculator) = calc match {
+	case (calc.brand == "hp" && true) => "financial"
+	case calc.brand == "hp" => "scientific"
+	case calc.brand == "hp" => "business"
+	case _ => "unknown"
+}
+
+println(calcType(new example01.Calculator("hp")))
+*/
+
+case class Calculator(brand: String, model: String)
+
+val hp20b = Calculator("hp", "20B");
+val hp20B = Calculator("hp", "20B");
+
+println("hp20b == hp20B" + (hp20b == hp20B))
+
+def calcType(calc: Calculator) = calc match {
+	case Calculator("hp", "20B") => "financial"
+	// scalac will detect "unreachable code" if not commented follows
+	// case Calculator("hp", "20B") => "scientific"
+	case Calculator("hp", "30B") => "business"
+	case Calculator(_, _) => "unknown type"
+}
+
+// Exception handling code not added now
+
+}
+}
