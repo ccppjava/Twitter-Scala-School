100bd5ab6da31d6afde439e352356834ed478e3a
yuqi <coolcnid@gmail.com>
2012-02-06 23:39:31 +0000

    Add java01 class which is a test and play with the story of: 	http://www.javaranch.com/campfire/StoryInner.jsp

--- example03.scala
+++ example03.scala
@@ -20,7 +20,34 @@ object example03 {
 
 
 		// Maps
+		Map(1 -> 2)
+		Map("foo" -> "bar")
+		val map = Map(1 -> Map("foo" -> "bar"))
+		// map with 1 -> 2 will automatically expands to (1, 2)
+
+		// Option is a container which may or may not hold something.
+		// basic interface for Option look like		
+		// trait Option[T] {def isDefined: Boolean def get: T def getOrElse(t: T): T}
+		// Option itself is generic and has two subclasses: Some[T] or None
+		val mapNumbers = Map(1 -> "one", 2 -> "two")
+
+		// Map.get use Option for it's return type. Option signifies to you
+		// that the method might not be able to return what you're asking for
+		// and that you should be prepared for that.
+		println(mapNumbers.get(2))
+		println(mapNumbers.get(3))
+
+		// Combinator are so-called because they are meant to be combined
+		// The output of one function is often suitable as the input for another
+		// map: Evaluates a function over each element in the list
+		// return a list with the same number of elements
+		println(numbers.map((i: Int) => i * 2))
+
+		// foreach: foreach is like map but returns nothing
+		// foreach is intended for side-effects only
+		println("foreach returns nothing: " + numbers.foreach((i: Int) => i * 2))
+
 	}
 }
 
-example03()
+// example03()
