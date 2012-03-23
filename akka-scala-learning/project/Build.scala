ce675679ce31c68d9701e506a735552b77445c66
yuqi <coolcnid@gmail.com>
2012-03-23 22:34:55 +0000

    the new type safe tutorial on AKKA http://typesafe.com/resources/getting-started/tutorials/getting-started-with-akka-scala.html

---
+++ akka-scala-learning/project/Build.scala
@@ -0,0 +1,18 @@
+import sbt._
+import sbt.Keys._
+
+object ProjectBuild extends Build {
+
+  lazy val root = Project(
+    id = "root",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "Akka Scala Learning",
+      organization := "home.learning",
+      version := "0.1-SNAPSHOT",
+      scalaVersion := "2.9.1",
+      resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
+      libraryDependencies += "com.typesafe.akka" % "akka-actor" % "2.0"
+    )
+  )
+}
