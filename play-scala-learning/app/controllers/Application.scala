bc2bfc457de52bf6ddc80cb543522a1467fec823
yuqi <coolcnid@gmail.com>
2012-03-29 13:18:47 +0100

    finish the same application according to the github: https://github.com/playframework/Play20 Hopefully will find good tutorial to continue

--- play-scala-learning/app/controllers/Application.scala
+++ play-scala-learning/app/controllers/Application.scala
@@ -2,11 +2,39 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+import views._
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-  
+    /**
+     * Describes the hello form.
+     */
+    val helloForm = Form (
+        tuple (
+            "name" -> nonEmptyText,
+            "repeat" -> number(min = 1, max = 100),
+            "color" -> optional(text)
+        )
+    )
+
+    // --Actions
+    
+    /**
+     * Home page
+     */
+    def index = Action {
+        Ok(html.index(helloForm))
+    }
+
+    /**
+     * Handles the form submission
+     */
+    def sayHello = Action { implicit request =>
+        helloForm.bindFromRequest.fold (
+            formWithErrors => BadRequest(html.index(formWithErrors)),
+            { case (name, repeat, color) => Ok(html.hello(name, repeat.toInt, color)) }
+        )
+    }
 }
